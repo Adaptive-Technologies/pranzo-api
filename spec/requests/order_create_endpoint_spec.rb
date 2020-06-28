9b072a62f20d98fc878df1c12cd5ebcb7c0d2fa6
Thomas <thomas@craftacademy.se>
2020-06-28 11:30:00 +0200

    adds tests to create order endpoint spec

--- spec/requests/order_create_endpoint_spec.rb
+++ spec/requests/order_create_endpoint_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe 'Post /api/orders' do
     expect(Order.last).to be_persisted
   end
 
+  it 'is expected to retun the order' do
+    expect(response_json['order']).to eq Order.last.as_json
+  end
+
+  it 'is expected to retun a success message' do
+    expect(response_json['message']).to eq 'Your order was submitted'
+  end
+
 end
