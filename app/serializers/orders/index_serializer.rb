a9de081bb8583ed1b0942226c348a75f9f9cf4ad
Thomas <thomas@craftacademy.se>
2020-07-13 16:26:19 +0200

    adds table attribute to order

--- app/serializers/orders/index_serializer.rb
+++ app/serializers/orders/index_serializer.rb
@@ -3,10 +3,6 @@ class Orders::IndexSerializer < ActiveModel::Serializer
   belongs_to :user, scope: :currrent_user, serializer: Users::ShowSerializer
   has_many :items, serializer: Items::ShowSerializer
 
-  def table
-
-  end
-  
   def time
     object.created_at.strftime('%H:%M')
   end
