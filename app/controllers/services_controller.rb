6535d14cb58eded741ecb620575e64e60719297c
Thomas <thomas@craftacademy.se>
2020-08-22 10:38:32 +0200

    extracts available services to separate module

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,13 +2,7 @@
 
 class ServicesController < ApplicationController
   def index
-    services = []
-    lunch_start = DateTime.now.beginning_of_day + (11.hours + 30.minutes)
-    lunch_end = DateTime.now.beginning_of_day + (14.hours + 30.minutes)
-    dinner_start = DateTime.now.beginning_of_day + 17.hours
-    dinner_end = DateTime.now.beginning_of_day + 20.hours
-    services.push('lunch') if DateTime.now.between?(lunch_start, lunch_end)
-    services.push('dinner') if DateTime.now.between?(dinner_start, dinner_end)
-    render json: { services: services }
+
+    render json: { services: ServicesService.list_available }
   end
 end
---
+++ app/services/services_service.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module ServicesService
+  def self.list_available
+    services = []
+    lunch_start = DateTime.now.beginning_of_day + (11.hours + 30.minutes)
+    lunch_end = DateTime.now.beginning_of_day + (14.hours + 30.minutes)
+    dinner_start = DateTime.now.beginning_of_day + 17.hours
+    dinner_end = DateTime.now.beginning_of_day + 20.hours
+    services.push('lunch') if DateTime.now.between?(lunch_start, lunch_end)
+    services.push('dinner') if DateTime.now.between?(dinner_start, dinner_end)
+    services
+  end
+end
