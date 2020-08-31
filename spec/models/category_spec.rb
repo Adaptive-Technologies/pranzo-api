e00ac2e1724d8783ec51ff0b3b9f4a9a6d8c91e8
Thomas <thomas@craftacademy.se>
2020-08-31 09:49:26 +0200

    adds specs for globalize functionality

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,15 +5,21 @@ RSpec.describe Category, type: :model do
       is_expected.to have_db_column(:name)
         .of_type(:string)
     }
+
+  end
+
+  describe 'Translations' do
     it {
-      is_expected.to have_db_column(:promo)
-        .of_type(:text)
+      is_expected.to respond_to(:translations)
+    }
+    
+    it {
+      is_expected.to respond_to(:promo)
     }
   end
 
   describe 'Factory' do
     it {
-      binding.pry
       expect(create(:category)).to be_valid
     }
   end
