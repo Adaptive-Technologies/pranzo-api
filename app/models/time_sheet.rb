c331a19c559cb1e85ae54a53068f555e8a1b7e55
Thomas <thomas@craftacademy.se>
2020-10-23 16:59:35 +0200

    modifies method for filtering time sheets

--- app/models/time_sheet.rb
+++ app/models/time_sheet.rb
@@ -9,7 +9,7 @@ class TimeSheet < ApplicationRecord
   }
 
   scope :for_previous_period, lambda {
-    where(date: (DateTime.now.months_ago(2).beginning_of_month + 14)..(DateTime.now.months_ago(2).end_of_month + 15))
+    where(date: (DateTime.now.months_ago(1).beginning_of_month + 14)..(DateTime.now.months_ago(1).end_of_month + 15))
   }
 
   private
