d10a4169c4f2220836e6b97bfccd1aeb70a16177
Thomas <thomas@craftacademy.se>
2020-11-01 10:25:25 +0100

    configures service for creeate and update actions

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## ResOS
 
 # API usage example
+Documentation: https://documenter.getpostman.com/view/3308304/SzzehLGp?version=latest
 
 ```ruby
 key = "OpBmGrJ5i03KBAq8pstw-4ZHX5YyyaOaGgmoV2zfcHK"
@@ -8,4 +9,36 @@ encoded_key = Base64.strict_encode64 key
 table_url = "https://api.resos.com/v1/tables"
 bookings_url = "https://api.resos.com/v1/bookings?fromDateTime=2020-10-30T00%3A00%3A00%2B01%3A00&toDateTime=2020-10-30T23%3A59%3A59%2B01%3A00&limit=2&skip=1"
 RestClient.get(url, headers={})
+```
+
+## ResOsService
+
+###  USAGE
+#### Collection of bookings:
+```ruby
+ResOsService.bookings
+```
+
+#### Collection of bookings for a specific date
+```ruby
+ResOsService.bookings(date: '2020-12-01')
+```
+You can also pass in `:limit` and `:skip`.
+
+#### Single booking:
+```ruby
+ResOsService.bookings(id: 'CRJE9Bbrqz2L8NbjT')
+```
+#### Create booking:
+```ruby
+ResOsService.bookings(type: :post, <pass in bookings details>)
+```
+
+#### Update booking
+```ruby
+ResOsService.bookings(
+  type: :put,
+  id: 'ip2ynBTToxvBGSdou'
+  <pass in bookings details>
+  )
 ```
--- app/services/res_os_service.rb
+++ app/services/res_os_service.rb
@@ -3,36 +3,70 @@
 module ResOsService
   key = 'OpBmGrJ5i03KBAq8pstw-4ZHX5YyyaOaGgmoV2zfcHK'
   encoded_key = Base64.strict_encode64 key
-  HEADERS = { Authorization: "Basic #{encoded_key}" }.freeze
+  HEADERS = {
+    Authorization: "Basic #{encoded_key}"
+  }.freeze
   API_URL = 'https://api.resos.com/v1'
+
   def self.tables
     response = RestClient.get(API_URL + '/tables', headers = HEADERS)
     JSON.parse response.body.force_encoding('UTF-8')
   end
 
-  # USAGE
-  # Collection of bookings:
-  # ResOsService.bookings
+  def self.bookings(options = {})
+    method = (options[:type] || :get)
+    case method
+    when :post
+      payload = booking_options(options).to_json
+      headers = HEADERS.deep_dup.merge!(content_type: :json, accept: :json)
+    when :put
+      raise StandardError, 'No booking id given' unless options[:id]
 
-  # Single booking:
-  # ResOsService.bookings(id: 'CRJE9Bbrqz2L8NbjT')
-  # Createe booking:
-  # ResOsService.bookings(type: :post, )
-
-  def self.bookings(options = { })
-    params = {
-      fromDateTime: (options.try(:from) || DateTime.now.beginning_of_day.to_s),
-      toDateTime: (options.try(:to) || DateTime.now.end_of_day.to_s),
-      limit: (options.try(:limit) || 2),
-      skip: (options.try(:skip) || 0)
-    }
-    payload = options.try(:payload)
-    response = RestClient::Request.execute(
-      method: (options[:type] || :get),
+      payload = booking_options(options).to_json
+      headers = HEADERS.deep_dup.merge!(content_type: :json, accept: :json)
+    when :get
+      params = bookings_period(options)
+      headers = HEADERS.deep_dup.merge!(params: params)
+    end
+    request = RestClient::Request.new(
+      method: method,
       url: API_URL + "/bookings#{options[:id] && '/' + options[:id]}",
       payload: payload,
-      headers: HEADERS.deep_dup.merge!(params: params)
+      headers: headers
     )
+    begin
+      response = request.execute
+    rescue StandardError => e
+      error = e
+    end
     JSON.parse response.body.force_encoding('UTF-8')
+  end
+
+  private
+
+  def self.booking_options(options = {})
+    booking_options = { date: (options[:date] || Date.today.to_s),
+                        time: (options[:time] || '12:15'),
+                        people: (options[:people] || 2),
+                        duration: (options[:duration] || 60),
+                        guest: { name: (options[:name] || 'Walk-in Guest'),
+                                 email: (options[:email] || 'info@bocado.se'),
+                                 notificationEmail: (options[:notificationEmail] || false),
+                                 notificationSms: (options[:notificationSms] || false),
+                                 phone: (options[:phone] || '') },
+                        languageCode: 'sv' }
+    booking_options
+  end
+
+  def self.bookings_period(options = {})
+    date = DateTime.parse(options[:date] || Date.today.to_s)
+    bookings_period =
+      {
+        fromDateTime: date.beginning_of_day.to_s,
+        toDateTime: date.end_of_day.to_s,
+        limit: (options[:limit] || 10),
+        skip: (options[:skip] || 0)
+      }
+    bookings_period
   end
 end
