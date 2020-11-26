f84eaf4f3f7b236ca21cd04abd048f35986cbb1d
Thomas <thomas@craftacademy.se>
2020-11-26 22:54:18 +0100

    adds endpoint to create transaaction on voucher

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
-
   namespace :admin do
     resources :users, only: [:index]
     resources :time_sheets, only: %i[create index], path: 'timesheets'
   end
   mount_devise_token_auth_for 'User', at: 'auth'
-  scope path: '/admin' do
-    resources :vouchers, only: [:create]
+  scope path: 'admin' do
+    resources :vouchers, only: %i[index create] do
+      resources :transactions, only: [:create]
+    end
     resources :orders, only: [:index]
   end
   scope path: 'api' do
--- spec/requests/admin/voucher_management/admin_can_create_voucher_spec.rb
+++ spec/requests/admin/voucher_management/admin_can_create_voucher_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative './credentials.rb'
+require_relative '../credentials.rb'
 
 RSpec.describe 'POST /admin/vouchers', type: :request do
   include_context 'credentials'
@@ -26,7 +26,7 @@ RSpec.describe 'POST /admin/vouchers', type: :request do
     it {
       expect(response).to have_http_status 422
     }
-    
+
     it 'is expected to return error message' do
       expect(response_json['message']).to eq "Value can't be blank"
     end
