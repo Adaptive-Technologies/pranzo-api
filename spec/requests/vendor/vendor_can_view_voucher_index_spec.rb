b0fc64d280a3095b3769aaccf920481c2a31ea97
tochman <thomas@craftacademy.se>
2022-10-10 15:09:27 +0200

    adds affiliate vouchers to index collection

--- spec/requests/vendor/vendor_can_view_voucher_index_spec.rb
+++ spec/requests/vendor/vendor_can_view_voucher_index_spec.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 
-require_relative './credentials.rb'
+require_relative './credentials'
 RSpec.describe 'GET /api/vendors/:vendor_id/vouchers', type: :request do
   include_context 'credentials'
+  let(:another_vendor) { create(:vendor) }
+  let!(:active_affiliate_vouchers) do
+    5.times do
+      create(:cash_voucher, issuer: another_vendor.system_user, active: true, value: 250, affiliate_network: true)
+    end
+  end
+  let!(:inactive_affiliate_vouchers) do
+    5.times do
+      create(:cash_voucher, issuer: another_vendor.system_user, active: false, value: 250, affiliate_network: true)
+    end
+  end
   let!(:s_vouchers) { 5.times { create(:servings_voucher, issuer: vendor_user, active: true) } }
-  let!(:c_vouchers) { 5.times { create(:cash_voucher, issuer: vendor_user, active: true, value: [250, 500, 1009].sample) } }
+  let!(:c_vouchers) do
+    5.times do
+      create(:cash_voucher, issuer: vendor_user, active: true, value: [250, 500, 1009].sample)
+    end
+  end
   let!(:other_vouchers) { 5.times { create(:voucher, active: true) } }
   let!(:transactions) do
     servings_vouchers_collection = Voucher.where(variant: 'servings')
@@ -13,14 +28,16 @@ RSpec.describe 'GET /api/vendors/:vendor_id/vouchers', type: :request do
       create(:transaction, voucher: servings_vouchers_collection[rand(0...4)])
     end
     5.times do
-      create(:transaction, voucher: cash_vouchers_collection[rand(0...4)], amount: [50, 120, 210].sample)
+      create(:transaction, voucher: cash_vouchers_collection[rand(0...4)], amount: [50, 75].sample)
     end
   end
+  
+  describe 'without affiliation' do
     before do
+      # vendor.reload
       get "/api/vendors/#{vendor.id}/vouchers",
           headers: valid_auth_headers_for_vendor_user
     end
-
     it {
       expect(response).to have_http_status 200
     }
@@ -28,4 +45,22 @@ RSpec.describe 'GET /api/vendors/:vendor_id/vouchers', type: :request do
     it 'is expected to include collection of vouchers' do
       expect(response_json['vouchers'].count).to eq 10
     end
+  end
+
+  describe 'without affiliation' do
+    let!(:affiliation) { another_vendor.affiliates << vendor }
+
+    before do
+      # vendor.reload
+      get "/api/vendors/#{vendor.id}/vouchers",
+          headers: valid_auth_headers_for_vendor_user
+    end
+    it {
+      expect(response).to have_http_status 200
+    }
+
+    it 'is expected to include collection of vouchers' do
+      expect(response_json['vouchers'].count).to eq 15
+    end
+  end
 end
