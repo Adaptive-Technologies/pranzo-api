f47c97dcfdb3a3b851a8dd66b74c45e5bcb55b09
tochman <thomas@craftacademy.se>
2022-10-10 21:10:22 +0200

    adds spec for new serializer

---
+++ spec/serializers/vendors_affiliate_serializer_spec.rb
@@ -0,0 +1,30 @@
+describe Vendors::AffiliateSerializer do
+  let(:vendor) { create(:vendor) }
+  let(:serialization) do
+    ActiveModelSerializers::SerializableResource.new(
+      vendor,
+      serializer: described_class
+    )
+  end
+  subject { JSON.parse(serialization.to_json) }
+
+  it 'is expected to wrap content in key reflecting model name' do
+    expect(subject.keys).to match ['vendor']
+  end
+  
+  it 'is expected to contain relevant keys' do
+    expected_keys = %w[id name primary_email users]
+    expect(subject['vendor'].keys).to match expected_keys
+  end
+
+  it 'is expected to have a specific structure' do
+    expect(subject).to match(
+      'vendor' => {
+        'id' => an_instance_of(Integer),
+        'name' => an_instance_of(String),
+        'primary_email' => a_string_including('@'),
+        'users' => an_instance_of(Array),
+      }
+    )
+  end
+end
