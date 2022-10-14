3b48759cf4f16fef2d08e0ddf98cb0f071782fed
tochman <thomas@craftacademy.se>
2022-10-14 08:52:59 +0200

    adds block with history of transactions

--- spec/requests/vendor/vendor_can_generate_transactions_report_spec.rb
+++ spec/requests/vendor/vendor_can_generate_transactions_report_spec.rb
@@ -3,12 +3,17 @@
 require_relative './credentials'
 
 RSpec.describe 'POST /api/vendors/:vendor_id/reports', type: :request do
-  let!(:servings_voucher) { create(:servings_voucher, issuer: vendor_user) }
-  let!(:cash_voucher) { create(:servings_voucher, issuer: vendor_user) }
-  let!(:old_transactions) do
-    
-  end
   include_context 'credentials'
+  let!(:servings_voucher) { create(:servings_voucher, issuer: vendor_user) }
+  let!(:cash_voucher) { create(:servings_voucher, value: 250, issuer: vendor_user) }
+  let!(:transactions) do
+    Timecop.freeze(1.month.ago.beginning_of_month)
+    create(:transaction, voucher: servings_voucher)
+    create(:transaction, voucher: cash_voucher, amount: 100)
+    Timecop.return
+    create(:transaction, voucher: servings_voucher)
+    create(:transaction, voucher: cash_voucher, amount: 50)
+  end
   before do
     post "/api/vendors/#{vendor.id}/reports", headers: valid_auth_headers_for_vendor_user
   end
