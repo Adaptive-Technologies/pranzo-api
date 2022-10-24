2af5d4da26918446b6f1c557963e9e4437110b38
tochman <thomas@craftacademy.se>
2022-10-24 23:19:46 +0200

    formats code and adds comments

--- config/patches/string.rb
+++ config/patches/string.rb
@@ -1,10 +1,11 @@
 class String
   def clean_up_xml_string
     <<~AID
-      Removes whitespaces and line breaks https://apidock.com/rails/String/squish
+      Removes whitespaces and line breaks .
       Also removes whitespace between tags.
-      Used in reading XML fixture files allowing us to use formatted XML
-      when stubbing Valvat requests.
+      Used in reading XML fixture files allowing 
+      us to use formatted XML when stubbing 
+      Valvat requests.
     AID
     squish.gsub('> <', '><')
   end
--- spec/vat_stubs.rb
+++ spec/vat_stubs.rb
@@ -1,6 +1,12 @@
 RSpec.configure do |config|
   config.before(:each) do
-    stub_request(:post, 'https://ec.europa.eu/taxation_customs/vies/services/checkVatService')
-      .to_return(status: 200, body: file_fixture('vat_response.xml').read.clean_up_xml_string, headers: {})
+    service_uri = 'https://ec.europa.eu/taxation_customs/vies/services/checkVatService'
+    fixture_data = file_fixture('vat_response.xml')
+                   .read
+                   .clean_up_xml_string
+    stub_request(:post, service_uri)
+      .to_return(status: 200,
+                 body: fixture_data,
+                 headers: {})
   end
 end
