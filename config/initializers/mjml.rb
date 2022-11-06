b197221764542417a3015f1f1f1da3668c48d03e
tochman <thomas@craftacademy.se>
2022-11-07 00:18:14 +0100

    fixes mjml formatting errors adds softer validation in mjml configuration file

--- config/initializers/mjml.rb
+++ config/initializers/mjml.rb
@@ -1,3 +1,9 @@
 Mjml.setup do |config|
   config.template_language = :erb # :erb (default), :slim, :haml, or any other you are using
+   # Optimize the size of your emails
+   config.beautify = false
+   config.minify = true
+ 
+   # Render MJML templates with errors
+   config.validation_level = "soft"
 end
