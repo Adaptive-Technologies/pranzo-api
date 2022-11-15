36d8a600e7c7589c5de1603339245faf326bc5b5
tochman <thomas@craftacademy.se>
2022-11-15 23:20:19 +0100

    adds sad path to reset password frow

--- spec/requests/user_can_update_password_using_token_spec.rb
+++ spec/requests/user_can_update_password_using_token_spec.rb
@@ -11,19 +11,31 @@ RSpec.describe 'POST /auth', type: :request do
 
   let(:missing_redirect_url_response) { { 'success' => false, 'errors' => ['Missing redirect URL.'] } }
 
-  before do
-    clear_emails
-    post '/auth/password', params: { email: user.email, redirect_url: 'http://example.com/reset' }
-
-    sleep 1
-    open_email 'new_user@mail.com'
+  describe 'with a redirect url' do
+    before do
+      post '/auth/password', params: { email: user.email, redirect_url: 'http://example.com/reset' }
+    end
+  
+    it 'is expected to respond with a success message' do
+      expect(response_json).to eq expected_response
+    end
+  
+    it 'is expected to send out an email' do
+      expect(email_queue).to eq 1
+    end
   end
 
-  it 'is expected to respond with a success message' do
-    expect(response_json).to eq expected_response
-  end
-
-  it 'is expected to send out an email' do
-    expect(email_queue).to eq 1
+  describe 'without a rediredct url' do
+    before do
+      post '/auth/password', params: { email: user.email }
+    end
+  
+    it 'is expected to respond with an error message' do
+      expect(response_json).to eq missing_redirect_url_response
+    end
+  
+    it 'is expected to NOT send out an email' do
+      expect(email_queue).to eq 0
+    end
   end
 end
