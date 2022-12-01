3e00c86a297f054053947cb7fa4bd6bc97cdcb77
tochman <thomas@craftacademy.se>
2022-12-01 08:47:04 +0100

    cleans up User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,6 @@
 
 class User < ActiveRecord::Base
   extend Devise::Models
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
@@ -13,6 +10,6 @@ class User < ActiveRecord::Base
   has_many :time_sheets, dependent: :destroy
   has_many :vouchers, foreign_key: :issuer_id, dependent: :destroy
   belongs_to :vendor, optional: true
-  # TODO: Decide if roles should be an arrau, meaning a user can have multiple roles
+  
   enum role: { consumer: 1, employee: 90, admin: 99, vendor: 100, system_user: 999 }
 end
